fc3ee607cb7e5aa79cce0fdc9602bb2a5042051c
Douglas Tavares <108306368+dogm19@users.noreply.github.com>
2022-10-25 22:31:20 -0300

    classe main

---
+++ lista/questao01/Pessoa/Main.java
@@ -0,0 +1,11 @@
+package Pessoa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args){
+		Pessoa01 p01 = new Pessoa01("Douglas", 2000, 2022, 1.78);
+		p01.apresentaPessoa();
+	}
+}
