f6e27ba502c22f99fe4f615ff4133168b65498db
Douglas Tavares <108306368+dogm19@users.noreply.github.com>
2022-12-06 13:43:12 -0300

    Create Principal.java

---
+++ Lista_valor-5,0/questao13/Principal.java
@@ -0,0 +1,13 @@
+package questao13;
+
+import questao04.Quest04;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Quest13 q13 = new Quest13();
+		
+		q13.ordemInverso();
+	}
+
+}
