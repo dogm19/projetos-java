00b72f98db076e1f5072e12e9c2a5c0dcf0f0f52
Douglas Tavares <108306368+dogm19@users.noreply.github.com>
2022-12-06 13:29:14 -0300

    Create Principal.java

---
+++ Lista_valor-5,0/questao06/Principal.java
@@ -0,0 +1,10 @@
+package questao06;
+import java.util.Scanner;
+public class Principal {
+	public static void main(String[] args) {
+		Quest06 q06 = new Quest06();
+		
+		q06.cauculoDeSalario();
+	}
+
+}
