482181e87012e74a233863b76ef4648af663baaa
Douglas Tavares <108306368+dogm19@users.noreply.github.com>
2022-12-06 13:40:39 -0300

    Create Quest12.java

---
+++ Lista_valor-5,0/questao12/Quest12.java
@@ -0,0 +1,15 @@
+
+package questao12;
+
+public class Quest12 { 
+
+	public void tela() {	
+		System.out.print("| Opção 1 - Aprender \n");
+        System.out.print("| Opção 2 - Escrever \n");
+        System.out.print("| Opção 3 - Falar \n");
+        System.out.print("| Opção 0 - Sair     \n");
+        System.out.print("Digite uma opção: ");
+        
+   
+	}         
+}
