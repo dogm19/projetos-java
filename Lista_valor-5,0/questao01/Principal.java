7bcb74f04cd6f7b1060a1ce9f668ffc3a5a062ee
Douglas Tavares <108306368+dogm19@users.noreply.github.com>
2022-12-06 13:17:39 -0300

    Create Principal.java

---
+++ Lista_valor-5,0/questao01/Principal.java
@@ -0,0 +1,11 @@
+package quastao01;
+
+public class Principal {
+	public static void main (String[] args) {
+		Quest01 q01 = new Quest01();
+		
+		System.out.println("Questão 01 cauculo de velocidade");
+		
+		q01.questaoUmCauculo();
+	}
+}
